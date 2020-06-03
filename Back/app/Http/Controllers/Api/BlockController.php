6a3873eca7e2642869f095dbc608973893969ab9
m-khalid <muhammad.khalid.5498@gmail.com>
2020-06-03 15:53:24 +0200

    token version

--- Back/app/Http/Controllers/Api/BlockController.php
+++ Back/app/Http/Controllers/Api/BlockController.php
@@ -11,17 +11,28 @@ use DB;
 
 class BlockController extends Controller
 {
+    public function __construct()
+        {
+            $request =  Request();
+            $user_data=User::where('token',$request->token)->first();
+            return $user_data;
+        }
     use ApiResponseTrait;
 
     public function block(Request $request)
     {
         $validator = Validator::make($request->all(),[
              'to_id' => 'required|integer',
+              'token'=>'required',
          ]);
-         $token=$request->header('token');
-         $user=User::where('token',$token)->first();
+         $user=$this->__construct();
+       if (!$user)
+       {
+       return $this->apiResponse(null,404);
+
+       }
          $friend=User::where('id',$request->to_id)->first();
-         if ($validator->fails()||empty($token)||empty($user)||empty($friend)) 
+         if ($validator->fails()||empty($user)||empty($friend)) 
          {      
             return $this->apiResponse(null,404);      
          }
@@ -45,9 +56,14 @@ class BlockController extends Controller
     {
         $validator = Validator::make($request->all(),[
             'to_id' => 'required|integer',
+            'token'=>'required',
         ]);
-        $token=$request->header('token');
-        $user=User::where('token',$token)->first();
+        $user=$this->__construct();
+        if (!$user)
+        {
+        return $this->apiResponse(null,404);
+        }
+        $token=$user->token;
         $friend=User::where('id',$request->to_id)->first();
         if ($validator->fails()||empty($token)||empty($user)||empty($friend)) 
         {      
@@ -67,12 +83,21 @@ class BlockController extends Controller
 
     public function list(Request $request)
     {
-        $token=$request->header('token');
-        $user=User::where('token',$token)->first();
-        if (empty($token)||empty($user)) 
-        {      
-           return $this->apiResponse(null,404);      
-        }
+            
+    $validator = Validator::make($request->all(),[
+        'token'=>'required',
+       ]);
+       if ($validator->fails()) 
+       {      
+        return $this->apiResponse(null,'404',$validator->messages()->first());    
+       }
+       $user=$this->__construct();
+       if (!$user)
+       {
+       return $this->apiResponse(null,404);
+       }
+
+      $token=$user->token;
         
          $data= DB::table('users')
             ->join('blocks', 'users.id', '=', 'blocks.friend_id')->where('user_id',$user->id)
--- Back/app/Http/Controllers/Api/RequestController.php
+++ Back/app/Http/Controllers/Api/RequestController.php
@@ -10,21 +10,31 @@ use DB;
 use App\User;
 class RequestController extends Controller
 {
+    public function __construct()
+        {
+            $request =  Request();
+            $user_data=User::where('token',$request->token)->first();
+            return $user_data;
+        }
     use ApiResponseTrait;
     ///send request ////
     public function request(Request $request)
     {
         $validator = Validator::make($request->all(),[
             'to_id' => 'required|integer',
+            'token'=>'required',
            ]);  
-           $token=$request->header('token');
-           $user=User::where('token',$token)->first();
-           if ($validator->fails()||empty($token)) 
+      
+           if ($validator->fails()) 
            {
-            return $this->apiResponse(null,404);    
+            return $this->apiResponse(null,'404',$validator->messages()->first());    
+   
            }
+           $user=$this->__construct();
+          
            if($user&&!empty(User::where('id',$request->to_id)->first())&&$user->id!=$request->to_id){
             $NewRequest = new Addrequest();
+            $token=$user->token;
             $NewRequest->reciever=$request->to_id;
             $NewRequest->sender=$user->id;
             $NewRequest->status=1;
@@ -39,16 +49,21 @@ class RequestController extends Controller
     ////////////////////////////////// count of Requests /////////////
     public function count_requet(Request $request)
     {
-        $token=$request->header('token');
-        $user=User::where('token',$token)->first();
-           if (empty($token)) 
+        $validator = Validator::make($request->all(),[
+          
+            'token'=>'required',
+           ]);
+           if ($validator->fails()) 
            {      
-            return $this->apiResponse(null,404);    
+            return $this->apiResponse(null,'404',$validator->messages()->first());    
            }
+           $user=$this->__construct();
+           
            if($user)
            {
-         $data['count']= Addrequest::where([['reciever', $user->id],['status',1]])->count('status');
-        return   $this->apiResponse($data);
+               $token=$user->token;
+            $data['count']= Addrequest::where([['reciever', $user->id],['status',1]])->count('status');
+           return   $this->apiResponse($data);
            }
            return $this->apiResponse(null,404);    
 
@@ -59,13 +74,19 @@ class RequestController extends Controller
 
     public function list(Request $request)
     {
-        $token=$request->header('token');
-        $user=User::where('token',$token)->first();
-        if (empty($token)) 
-        {
-         return $this->apiResponse(null,404);    
-        }
+        $validator = Validator::make($request->all(),[
+      
+            'token'=>'required',
+           ]);
+           if ($validator->fails()) 
+           {      
+            return $this->apiResponse(null,'404',$validator->messages()->first());    
+           }
+           $user=$this->__construct();
+    
+  
         if($user){
+            $token=$user->token;
             $data= DB::table('users')
             ->join('addrequests', 'users.id', '=', 'addrequests.sender')->where('reciever',$user->id)
             ->select('addrequests.sender','users.img','addrequests.created_at','users.username')->get();
